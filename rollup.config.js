6e5a6b005bc70d41077f59347eed45bc47f1c414
Dhaval Vira <dhavalveera@gmail.com>
2023-07-16 12:25:06 +0530

    dependency updated

--- rollup.config.js
+++ rollup.config.js
@@ -1,38 +1,38 @@
-import resolve from "@rollup/plugin-node-resolve";
-import babel from "@rollup/plugin-babel";
-import commonjs from "@rollup/plugin-commonjs";
-import { terser } from "rollup-plugin-terser";
-import pkg from "./package.json";
+import resolve from '@rollup/plugin-node-resolve'
+import babel from '@rollup/plugin-babel'
+import commonjs from '@rollup/plugin-commonjs'
+import terser from '@rollup/plugin-terser'
+import pkg from './package.json'
 
 export default {
-  input: "./index.js",
+  input: './index.js',
   output: [
     {
       file: pkg.main,
-      format: "cjs",
-      exports: "named",
+      format: 'cjs',
+      exports: 'named',
     },
     {
       file: pkg.module,
-      format: "esm",
-      exports: "named",
+      format: 'esm',
+      exports: 'named',
     },
   ],
   // external: builtins,
-  external: ["react"],
+  external: ['react'],
   plugins: [
     resolve(),
     babel({
-      exclude: "node_modules/**",
-      babelHelpers: "runtime",
+      exclude: 'node_modules/**',
+      babelHelpers: 'runtime',
     }),
     // resolve({
     //   preferBuiltins: true
     // }),
     // nodeResolve(),
     commonjs({
-      extensions: [".js", ".ts", ".tsx"],
+      extensions: ['.js', '.ts', '.tsx'],
     }),
     terser(),
   ],
-};
+}
